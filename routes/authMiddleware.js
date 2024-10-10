94cee10e449fa1425fe9f91d556bac8f85b17f95
Ninad <10viranininad@gmail.com>
2024-10-10 14:30:56 -0400

    changes in authMiddleware.js

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'pragnesh-BAAPU';  // Same secret as above
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '');
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
@@ -9,6 +9,11 @@ const authMiddleware = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(decoded.user_id);
+
+        if (!user) {
+            return res.status(401).json({ message: 'User no longer exists' });
+        }
         req.user = decoded;
         next();
     } catch (err) {
